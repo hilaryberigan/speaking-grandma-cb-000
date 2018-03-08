51879fce7a30a524f0b20addbdeefd52c7342573
Hilary Berigan <berigan.hilary@gmail.com>
2018-03-08 23:21:11 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -13,7 +13,9 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 def speak_to_grandma(speech)
-  if speech.upcase == speech
+  if speech == "I LOVE YOU GRANDMA!"
+    puts "I LOVE YOU TOO PUMPKIN"
+  elsif speech.upcase == speech
     puts "NO, NOT SINCE 1938!"
   else
     puts "HUH?! SPEAK UP, SONNY!"
